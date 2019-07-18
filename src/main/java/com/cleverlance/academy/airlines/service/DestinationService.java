8753e796b5e7bd96543186204523e20a743fd1d6
schednie <soultips239@gmail.com>
2019-07-18 14:36:51 +0200

    fix

--- src/main/java/com/cleverlance/academy/airlines/service/DestinationService.java
+++ src/main/java/com/cleverlance/academy/airlines/service/DestinationService.java
@@ -45,18 +45,24 @@ public class DestinationService implements IDestinationService {
         return destinationRepository.findById(id);
     }
 
-    @PostConstruct
+    @Scheduled(fixedRate = 36000000)
     private void loadFromThirdParty() {
         Map<String, Destination> persistedDestinations = destinationRepository.findAll().stream()
                 .collect(Collectors.toMap(Destination::getCode, d -> d));
         Map<String, Destination> freshDestinations = airportClient.getAllAirports().stream()
                 .collect(Collectors.toMap(Destination::getCode, d -> d));
 
-        freshDestinations.keySet().removeAll(persistedDestinations.keySet());
-        destinationRepository.saveAll(freshDestinations.values());
-
-
+        removeOld(new HashMap<>(persistedDestinations), freshDestinations);
+        addNew(new HashMap<>(freshDestinations), persistedDestinations);
     }
 
-    private void addDifference()
+    private void removeOld(Map<String, Destination> persisted, Map<String, Destination> fresh) {
+        persisted.keySet().removeAll(fresh.keySet()); // 'persisted' now contains deleted airports from API
+        destinationRepository.deleteAll(persisted.values());
+    }
+
+    private void addNew(Map<String, Destination> persisted, Map<String, Destination> fresh) {
+        fresh.keySet().removeAll(persisted.keySet()); // 'fresh' now contains new airports from API
+        destinationRepository.saveAll(fresh.values());
+    }
 }
